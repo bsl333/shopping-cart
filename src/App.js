4b6e3435a3605c6495acfaf771e5b77b860dd0e7
Branden <b.lowe333@gmail.com>
2018-08-13 22:16:32 -0700

    modularized cart-item and cart-items

--- src/App.js
+++ src/App.js
@@ -5,6 +5,11 @@ import CartHeader from './components/header'
 import CartFooter from './components/footer'
 import CartItems from './components/cart-items'
 
+const cartItemsList = [
+  { id: 1, product: { id: 40, name: 'Mediocre Iron Watch', priceInCents: 399 }, quantity: 1 },
+  { id: 2, product: { id: 41, name: 'Heavy Duty Concrete Plate', priceInCents: 499 }, quantity: 2 },
+  { id: 3, product: { id: 42, name: 'Intelligent Paper Knife', priceInCents: 1999 }, quantity: 1 },
+]
 
 class App extends Component {
   render() {
@@ -12,8 +17,8 @@ class App extends Component {
     return (
       <div> 
         <CartHeader />
-        <CartItems />
-        <CartFooter />
+        <CartItems items={cartItemsList} />
+        <CartFooter copyright="2016"/>
       </div>
     )
     // return (
--- src/components/cart-items.js
+++ src/components/cart-items.js
@@ -1,9 +1,22 @@
 import React from 'react'
+import CartItem from './cart-item'
 
-const CartItems = () => (
+const CartItems = ({ items }) => (
   <div className="container">
-    <h1>Items</h1>
+    <h1>Cart Items</h1>
+    <div className="list-group">
+      <div className="list-group-item">
+        <div className="row">
+          <div className="col-md-8">Product</div>
+          <div className="col-md-2">Price</div>
+          <div className="col-md-2">Quantity</div>
+        </div>
+      </div>
+      {items.map(item => <CartItem key={item.id} item={item} />)}
+    </div>
   </div>
+
+
 )
 
 export default CartItems
